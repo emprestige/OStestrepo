015624a708bbaa6d46289eec6adf287bcd350449
Em Prestige <emiel200@gmail.com>
2023-10-04 11:00:39 +0100

    Update report.R

--- analysis/report.R
+++ analysis/report.R
@@ -5,7 +5,7 @@ library("arrow")
 df_input <- read_feather(
     here::here("output", "input.feather"))
 
-plot_age <- ggplot(data = df_input, aes(df_input$age)) + geom_histogram()
+plot_age <- ggplot(data = df_input, aes(age)) + geom_histogram()
 
 ggsave(
     plot = plot_age,
