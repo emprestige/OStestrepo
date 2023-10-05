06d6f653714d362457f9581afe6aae500b4d5859
Em Prestige <emiel200@gmail.com>
2023-10-05 17:32:56 +0100

    Update dataset_definition.py

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -24,7 +24,7 @@ dataset.cause_of_death = last_ons_death.cause_of_death_01 #cause of death
 
 #import ethnicity codelist
 ethnicity_codelist = codelist_from_csv(
-    "codelists/opensafely-ethnicity-snomed-0removed",
+    "codelists/opensafely-ethnicity-snomed-0removed.csv",
     column="snomedcode",
     category_column="Grouping_6",
 )
