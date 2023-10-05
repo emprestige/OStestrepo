40ed1d8b18dc1efe72cd0a29dce1c131e9c14627
Em Prestige <emiel200@gmail.com>
2023-10-05 17:05:50 +0100

    Update dataset_definition.py

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -24,7 +24,7 @@ dataset.cause_of_death = last_ons_death.cause_of_death_01 #cause of death
 
 #import ethnicity codelist
 ethnicity_codelist = codelist_from_csv(
-    "codelists/ethnicity_codelist_with_categories.csv",
+    "ethnicity_codelist_with_categories.csv",
     column="snomedcode",
     category_column="Grouping_6",
 )
