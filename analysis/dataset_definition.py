bbc57f9973b0fc9bcfd0c0bafe335a7e6e9dfc31
Em Prestige <emiel200@gmail.com>
2023-10-06 16:35:30 +0100

    Add more characteristics to dummy data

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -1,14 +1,23 @@
 from ehrql import create_dataset, case, when
-from ehrql.tables.beta.tpp import patients, medications, ons_deaths
-from ehrql.tables.beta.core import clinical_events
+from ehrql.tables.beta.tpp import ( 
+  patients, 
+  medications,
+  ons_deaths,
+  addresses, 
+  clinical_events,
+  practice_registrations
+)
 from ehrql import codelist_from_csv
 
 dataset = create_dataset()
 
+#deine population size
 dataset.configure_dummy_data(population_size=1000)
 
+#define population 
 dataset.define_population(patients.date_of_birth.is_on_or_before("1999-12-31")) #get patients after certain date
 
+#get patients ages and age groups
 dataset.age = patients.age_on("2019-09-01") #get patients age on defined date
 dataset.age_band = case(
         when(dataset.age < 20).then("0-19"),
@@ -44,12 +53,21 @@ dataset.latest_ethnicity_group = dataset.latest_ethnicity_code.to_category(
     ethnicity_codelist
 )
 
-asthma_codes = ["39113311000001107", "39113611000001102"]
-latest_asthma_med = (
-    medications.where(medications.dmd_code.is_in(asthma_codes))
-    .sort_by(medications.date)
-    .last_for_patient()
+#get patients IMD rank 
+dataset.imd = addresses.for_patient_on("2023-01-01").imd_rounded
+
+#calculate IMD quintile
+dataset.imd_quintile = case(
+    when((dataset.imd >=0) & (dataset.imd < int(32844 * 1 / 5))).then("1 (most deprived)"),
+    when(dataset.imd < int(32844 * 2 / 5)).then("2"),
+    when(dataset.imd < int(32844 * 3 / 5)).then("3"),
+    when(dataset.imd < int(32844 * 4 / 5)).then("4"),
+    when(dataset.imd < int(32844 * 5 / 5)).then("5 (least deprived)"),
+    default="unknown"
 )
 
-dataset.asthma_med_date = latest_asthma_med.date
-dataset.asthma_med_code = latest_asthma_med.dmd_code
+#get rural/urban classification 
+dataset.rural_urban = addresses.for_patient_on("2023-01-01").rural_urban_classification
+
+#get patietns practice's pseudonymised identifier
+dataset.practice = practice_registrations.for_patient_on("2023-01-01").practice_pseudo_id
--- analysis/report.R
+++ analysis/report.R
@@ -1,12 +1,20 @@
 library("tidyverse")
 library("here")
 library("arrow")
+library("ggplot2")
+library("data.table")
 
 df_input <- read_feather(
     here::here("output", "input.arrow"))
 
-# plot_age <- ggplot(data = df_input, aes(age)) + geom_histogram()
+# df2 <- rlang::duplicate(df_input)
+# df2 <- as.data.table(df2)
+# df2 <- df2[is.na(date_of_death) == F, ]
 # 
+# summary(lm(age ~ latest_ethnicity_group + imd + rural_urban, data = df2))
+
+# plot_age <- ggplot(data = df_input, aes(age)) + geom_histogram()
+
 # ggsave(
 #     plot = plot_age,
 #     filename = "descriptive.png", path = here::here("output"),
