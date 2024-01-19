c882a42d6bd5b17b3e8f5cb7f036764f49589f6b
Em Prestige <emiel200@gmail.com>
2024-01-19 16:36:08 +0000

    update report

--- analysis/report_infants.R
+++ analysis/report_infants.R
@@ -7,9 +7,10 @@ library("data.table")
 df_input <- read_feather(
     here::here("output", "input_manipulated_infants.arrow"))
 
-# plot_age <- ggplot(data = df_input, aes(age)) + geom_histogram()
+plot_age <- ggplot(data = df_input, aes(age, frequency(age))) + geom_col(width = 0.9) +
+  xlab("Age (Months)") + ylab("Frequency")
 
-# ggsave(
-#     plot = plot_age,
-#     filename = "descriptive.png", path = here::here("output"),
-# )
+ggsave(
+    plot = plot_age,
+    filename = "descriptive_infants.png", path = here::here("output"),
+)
